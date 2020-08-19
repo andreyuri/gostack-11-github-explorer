0d9c9342543f43ece62271538768015bb9271c7b
André Yuri <andre_yuri@live.com>
2020-08-18 21:48:35 -0300

    Navigation between routes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
 
-// import { Container } from './styles';
+interface RouteParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RouteParams>();
+
+  return <h1>Repository {params.repository}</h1>;
 };
 
 export default Repository;
